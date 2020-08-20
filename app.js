f17ee206745fe48ddbf4ee85ae319bd1db810901
Chandrjeet <chrajpoot60@gmail.com>
2020-08-20 11:35:55 +0530

    cookies with Basic Authentication

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
+const { signedCookie } = require('cookie-parser');
 
 const url = 'mongodb://localhost:27017/confusion';
 const connect = mongoose.connect(url);
@@ -29,7 +30,51 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-78004-34567'));//signing cookies with secret key
+
+function auth(req, res, next) {
+  console.log(req.signedCookies);
+
+  if(!req.signedCookies.user) {
+      //extracting authorization header
+    var authHeader = req.headers.authorization;
+    //checking authHeader is not null
+    if(!authHeader) {
+      var err = new Error('You are not authenticated:');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
+    //extracting username and password from authHeader
+    var auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+    var username = auth[0];
+    var password = auth[1];
+    
+    if(username == "admin" && password == "password") {
+      res.cookie('user', 'admin', {signed: true});
+      next();//move to accesseble page
+    }
+    else {
+      var err = new Error('You are not authenticated:');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
+  }
+  else {
+    if(req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated:');
+      err.status = 401;
+      return next(err);
+    }
+  }  
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
